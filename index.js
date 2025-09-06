6e7731fe61bf8943744f5245c0ce751cd22ee11d
StokedTBH <112590070+StokedTBH@users.noreply.github.com>
2025-09-05 19:58:43 -0500

    Worked with altering elements and logging variables again.

--- index.js
+++ index.js
@@ -6,3 +6,45 @@
 
 document.getElementById("header_number_one").textContent = `Hello, World`;
 document.getElementById("paragraph_number_one").textContent = `I like coding!`;
+
+let x = 100;
+let y;
+y = 200;
+
+console.log(y);
+
+let age = 22;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 3.31
+console.log(gpa);
+
+console.log(`You are ${age} years old, and your gpa is ${gpa}`);
+console.log(`The price is ${price}`);
+
+let firstName = "Ethan";
+let favoriteFood = "burgers";
+let email = "12345678@gmail.com"
+console.log(typeof firstName);
+console.log(typeof favoriteFood);
+console.log(`Your email is ${email}`);
+
+let online = false;
+console.log(`${firstName} is online: ${online}}`);
+let forSale = true;
+console.log(`Is this car for sale?: ${forSale}`);
+let isStudent = true;
+console.log(`Enrolled: ${isStudent}`)
+
+let lastName = "Stokey";
+
+console.log(`${firstName} ${lastName} is ${age} years old, and he is a student: ${isStudent}`)
+document.getElementById("paragraph_number_one").textContent = `${firstName} ${lastName} is ${age} years old, and he is a student: ${isStudent}`;
+
+/*
+Strings, numbers (ints and demcimal numbers 
+all included), booleans
+*/
