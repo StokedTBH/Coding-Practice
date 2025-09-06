933a4654e10bc6e7043b24dfd0c63ff3949fd16c
StokedTBH <112590070+StokedTBH@users.noreply.github.com>
2025-09-05 19:45:25 -0500

    Working with window alerts and console.

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log(`Hello, World!`);
+
+// Alerts baby
+//window.alert(`This is an alert!`);
+//window.alert(`This is another alert!`);
+
+document.getElementById("header_number_one").textContent = `Hello, World`;
+document.getElementById("paragraph_number_one").textContent = `I like coding!`;
